57e1a85f3f24ce049ed55902a99036d53f918f42
Elizabeth Alcalá <elizabethalcala28@gmail.com>
2021-02-27 19:50:03 -0500

    feat: create task repository

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TaskRepositoy } from './task.repository';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TaskRepositoy])],
   controllers: [TasksController],
   providers: [TasksService],
 })
